1d400614285a1f4c74e582b6b15cfd9b22ebf8d5
graydonpleasants <1348557+graydonpleasants@users.noreply.github.com>
2026-02-24 02:02:39 +0000

    Refactor bulk_load to use parallel helper functions

--- src/utils/parallel.rs
+++ src/utils/parallel.rs
@@ -50,3 +50,73 @@ where
         collection.iter_mut().for_each(f);
     }
 }
+
+/// Helper for parallel flat_map
+#[inline]
+pub fn par_flat_map<T, F, U, R>(slice: &[T], f: F) -> Vec<R>
+where
+    T: Sync,
+    F: Fn(&T) -> U + Sync + Send,
+    U: IntoIterator<Item = R>,
+    R: Send,
+{
+    #[cfg(all(feature = "parallel", not(target_arch = "wasm32")))]
+    {
+        slice.par_iter().flat_map_iter(f).collect()
+    }
+    #[cfg(any(not(feature = "parallel"), target_arch = "wasm32"))]
+    {
+        slice.iter().flat_map(f).collect()
+    }
+}
+
+/// Helper for parallel unstable sort
+#[inline]
+pub fn par_sort_unstable<T: Ord + Send>(slice: &mut [T]) {
+    #[cfg(all(feature = "parallel", not(target_arch = "wasm32")))]
+    {
+        slice.par_sort_unstable();
+    }
+    #[cfg(any(not(feature = "parallel"), target_arch = "wasm32"))]
+    {
+        slice.sort_unstable();
+    }
+}
+
+/// Helper for parallel zip and for_each
+#[inline]
+pub fn par_zip_for_each<A, B, F>(a: &mut [A], b: &[B], f: F)
+where
+    A: Send,
+    B: Sync,
+    F: Fn(&mut A, &B) + Sync + Send,
+{
+    #[cfg(all(feature = "parallel", not(target_arch = "wasm32")))]
+    {
+        a.par_iter_mut()
+            .zip(b.par_iter())
+            .for_each(|(x, y)| f(x, y));
+    }
+    #[cfg(any(not(feature = "parallel"), target_arch = "wasm32"))]
+    {
+        a.iter_mut().zip(b.iter()).for_each(|(x, y)| f(x, y));
+    }
+}
+
+/// Helper for parallel consume, enumerate, map and collect
+#[inline]
+pub fn par_into_enumerate_map<T, F, U>(vec: Vec<T>, f: F) -> Vec<U>
+where
+    T: Send,
+    F: Fn((usize, T)) -> U + Sync + Send,
+    U: Send,
+{
+    #[cfg(all(feature = "parallel", not(target_arch = "wasm32")))]
+    {
+        vec.into_par_iter().enumerate().map(f).collect()
+    }
+    #[cfg(any(not(feature = "parallel"), target_arch = "wasm32"))]
+    {
+        vec.into_iter().enumerate().map(f).collect()
+    }
+}
