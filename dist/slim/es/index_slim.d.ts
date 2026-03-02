5763fdaaaaf46896d699e9b06333fd4111638847
graydonpleasants <1348557+graydonpleasants@users.noreply.github.com>
2026-03-02 23:32:27 +0000

    Optimize python allocations, and add publish workflows

--- dist/slim/es/index_slim.d.ts
+++
@@ -1,3 +0,0 @@
-import * as scalarExports from "../pkg-scalar/geo_polygonize.js";
-export * from "../pkg-scalar/geo_polygonize.js";
-export declare function initBest(scalarModule: any, simdModule: any): Promise<typeof scalarExports>;
--- dist/standard/es/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as exports from "../pkg-scalar/geo_polygonize.js";
-export * from "../pkg-scalar/geo_polygonize.js";
-export default function init(_input?: any): Promise<typeof exports>;
